7ba5633a3aeddd6b0f2956aec191e4a4204613f6
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-09-21 15:07:42 +0900

    180921 UnityTerakoyaAgain -p28

---
+++ UnityTerakoyaAgain/Assets/Scripts/BonzMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bonz
+{
+    public class BonzMover : MonoBehaviour
+    {
+        private Vector3 startPosition;
+        private readonly float moveRange = 2.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            Transform transform = GetComponent<Transform>();
+
+            startPosition = this.transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 position = new Vector3(
+                    Random.Range(-moveRange, moveRange),
+                    Random.Range(-moveRange, moveRange)
+                );
+            transform.position = startPosition + position;
+        }
+    }
+}
